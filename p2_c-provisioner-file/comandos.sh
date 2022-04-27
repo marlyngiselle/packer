5e3a4777594c5304df662df29c7fa421819d77d7
marlyngiselle <100991825+marlyngiselle@users.noreply.github.com>
2022-04-27 21:16:09 +0200

    Update comandos.sh

--- p2_c-provisioner-file/comandos.sh
+++ p2_c-provisioner-file/comandos.sh
@@ -3,7 +3,7 @@ sudo apt update
 sudo apt install -y nginx
 systemctl start nginx
 systemctl enable nginx
+sudo ufw enable
 sudo ufw allow ssh
 sudo ufw allow http
 sudo ufw allow https
-sudo ufw enable
